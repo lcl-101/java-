7786bac91e98873258b33cd251a580974e1fa05d
lichenglong <554675188@qq.com>
2020-09-27 17:27:20 +0800

    ++

--- 练习/day1/code007/src/PrimaryTypes.java
+++ 练习/day1/code007/src/PrimaryTypes.java
@@ -1,6 +1,25 @@
 public class PrimaryTypes {
     public static void main(String[] args){
-        int bigNum = 999999999999999;
+//        int bigNum = 999999999999999;
         long number = 999999999999999L;
+
+        System.out.println(1/3.0);
+        System.out.println(1 < 2);
+        System.out.println(1 > 2);
+
+        System.out.println("AB");
+        System.out.println('A');
+
+        byte byteNum = 127;
+        System.out.println(byteNum);
+
+        short shortNum = -30000;
+        System.out.println(shortNum);
+
+        int inrNum = 300000;
+        System.out.println(inrNum);
+
+        long longVar = 99;
+        System.out.println(longVar);
     }
 }
