53f77c4e079784166cd00ac7587d44a9b25210db
lichenglong <554675188@qq.com>
2020-10-09 18:55:56 +0800

    ++

--- 练习/day1/code007/src/OprtValue.java
+++ 练习/day1/code007/src/OprtValue.java
@@ -3,5 +3,21 @@ public class OprtValue {
         int a = 100;
         System.out.println(a + 1);
         System.out.println(a);
+
+        a = a + 6;
+        System.out.println(a);
+
+        int num = -10;
+        System.out.println(num % 2);
+        System.out.println(num % -3);
+        System.out.println(num % 4);
+        System.out.println(num % 5);
+        System.out.println(num % -6);
+
+        int c = 10;
+        int b = 3;
+        double d = 3.0;
+        System.out.println(c/b);
+        System.out.println(c/d);
     }
 }
