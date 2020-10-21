24ae6027a5cc8d2427abc7559545017a3de1a3cd
lichenglong <554675188@qq.com>
2020-10-21 19:47:04 +0800

    ++

--- 练习/day2/code014/src/CalcDivfor.java
+++ 练习/day2/code014/src/CalcDivfor.java
@@ -4,10 +4,23 @@ public class CalcDivfor {
         int divisor = 3;
 
         int found = 0;
-        for(int i=0;i< 100 && found < 10;i++){
+        int toBeFound = 10;
+//        for(int i=0;i< 100 && found < 10;i++){
+//            if(divided % divisor == 0){
+//                System.out.println(divided+"可以整除" + divisor + ",商为:" + (divided/divisor));
+//                found++;
+//            }
+//            divided++;
+//        }
+
+        for(int i=0;i< 100;i++){
             if(divided % divisor == 0){
                 System.out.println(divided+"可以整除" + divisor + ",商为:" + (divided/divisor));
                 found++;
+            }
+            if(found >= toBeFound){
+                System.out.println("已经找到" + toBeFound + "个数, 循环退出");
+                break;
             }
             divided++;
         }
