80f0f46e583b1e4aa8900c5321f6e90231538301
lichenglong <554675188@qq.com>
2020-10-22 18:36:27 +0800

    ++

---
+++ 练习/day2/code015/src/Example.java
@@ -0,0 +1,14 @@
+public class Example {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 9; i++) {
+            String line = "";
+            for (int j = 1; j <= 9; j++) {
+                if(j > i){
+                    break;
+                }
+                line += i + "*" + j + "=" + (i * j) + "\t";
+            }
+            System.out.println(line);
+        }
+    }
+}
