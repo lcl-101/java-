4d274936698bc32da509e758f730f901bff09437
lichenglong <554675188@qq.com>
2020-10-22 18:55:45 +0800

    ++

---
+++ 练习/day2/code015/src/FindNDivEndLess.java
@@ -0,0 +1,18 @@
+public class FindNDivEndLess {
+    public static void main(String[] args) {
+        int n = 10;
+
+        int dividend = 100;
+        int divisor = 2000000000;
+
+        int found = 0;
+
+        while (found < n){
+            if(dividend % divisor == 0){
+                System.out.println(dividend + "可以整除" + divisor + ". 商为" + (dividend/divisor));
+                found++;
+            }
+            dividend++;
+        }
+    }
+}
