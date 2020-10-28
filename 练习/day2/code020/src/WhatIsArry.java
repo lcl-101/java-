f43193b900e63c0ae8f93df015106c4fffe530c6
lichenglong <554675188@qq.com>
2020-10-28 18:40:08 +0800

    ++

---
+++ 练习/day2/code020/src/WhatIsArry.java
@@ -0,0 +1,9 @@
+public class WhatIsArry {
+    public static void main(String[] args) {
+        int[] book = new int[10];
+
+        System.out.println(book[0]);
+        System.out.println(book[1]);
+
+    }
+}
