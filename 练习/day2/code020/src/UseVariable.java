b024ab8faa7b5cec244bd30d0a2e1cd985335751
lichenglong <554675188@qq.com>
2020-10-28 18:19:02 +0800

    ++

---
+++ 练习/day2/code020/src/UseVariable.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class UseVariable {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int a;
+        System.out.println("创建的变量a,输入一个整数: ");
+        a = in.nextInt();
+        System.out.println("给a赋值,a的值为" + a + ", 请再次输入一个整数:");
+
+        a = in.nextInt();
+        System.out.println("再次给a赋值,现在a的值为" + a);
+
+    }
+}
