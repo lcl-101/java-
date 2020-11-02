7261acf21a3517fc206c3e8e850a66307e2d7150
lichenglong <554675188@qq.com>
2020-11-02 18:21:51 +0800

    ++

---
+++ 练习/day3/code/src/MerchanddisablebaribleEmpales.java
@@ -0,0 +1,15 @@
+public class MerchanddisablebaribleEmpales {
+    public static void main(String[] args) {
+        //用四个变量描述商品1
+        String m1Name = "茉莉花茶叶20包";
+        String m1Id = "000099518";
+        int m1count = 100;
+        double m1Price = 99.9;
+
+        //用四个变量描述商品2
+        String m2Name = "可口可乐,300ml";
+        String m2Id = "000099519";
+        int m2count = 100;
+        double m2Price = 3.0;
+    }
+}
---
+++ 练习/day3/code/src/Merchdise.java
@@ -0,0 +1,9 @@
+// >> TODO 一个类public class开头, public class代表这个类是公共类, 类名必须和文件名相同.
+// >> TODO public class后面紧跟着类名, 然后是一对括号的类体
+public class Merchdise {
+    // >> TODO 类体重可以定义描述这个类的属性的变量, 我们称之为成员变量(member variable)
+    // >> TODO 每个成员变量的定义;结束
+    String name;
+}
+
+// >> TODO 上面这整个类, 其实就是创建了一个模板,描述了一种我们需要的数据类型.
