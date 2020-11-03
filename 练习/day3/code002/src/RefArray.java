42bcfdd23df99016779a99a016cc6bae607be776
lichenglong <554675188@qq.com>
2020-11-03 19:24:12 +0800

    ++

--- 练习/day3/code002/src/RefArray.java
+++ 练习/day3/code002/src/RefArray.java
@@ -5,8 +5,10 @@ public class RefArray {
         merchandises[1] = new Merchandise();
         merchandises[0].name = "笔记本";
         merchandises[0].count = 1;
+        merchandises[0].id = "000001";
         System.out.println(merchandises[0].name);
         System.out.println(merchandises[0].count);
+        System.out.println(merchandises[0].id);
 
         System.out.println(merchandises[2]);
     }
